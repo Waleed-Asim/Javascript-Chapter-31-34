0e25613bf339dd996a3c1b1592d204c03f7cf3e1
Waleed-Asim <83092895+Waleed-Asim@users.noreply.github.com>
2021-06-01 02:31:00 +0500

    Add initial files

---
+++ app.js
@@ -0,0 +1,58 @@
+// Q1
+
+// var dateToday = new Date();
+// console.log(dateToday);
+
+// Q2
+
+// var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug"];
+// var now = new Date();
+// var theMonth = now.getMonth();
+// var nameOfMonth = months[theMonth];
+// alert("Current month: " + nameOfMonth);
+
+// Q3
+
+// var days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+// var now = new Date();
+// var theDay = now.getDay();
+// var dayToday = days[theDay];
+// alert("Today is " + dayToday);
+
+// Q4
+
+// var days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+// var now = new Date();
+// var theDay = now.getDay();
+// var dayToday = days[theDay];
+
+// if(dayToday === "Sat" || "Sun"){
+//     alert("It's a fun day");
+// }
+
+// else{
+//     alert("Go back to work!")
+// }
+
+// Q6
+
+// var newDate = new Date();
+// var miliSec = newDate.getTime();
+// var min = miliSec * 59;
+// console.log("Current Date " + newDate);
+// console.log("Milisec since Jan 1970 " + miliSec);
+// console.log("Minutes since Jan 1970 " + min);
+
+// Q7
+
+// Q13
+
+// var UserDate = prompt("Enter date of birth", "Jan 1 1990");
+// var date = new Date(UserDate);
+// var today = new Date();
+// var diff = today.getTime() - date.getTime();
+// var year = diff / (1000*60*60*24*365);
+
+// console.log(Math.floor(year));
+
+
